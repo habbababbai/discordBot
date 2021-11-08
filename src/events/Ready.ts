6c6201085a58b138959af9a7c753f6af0803ea99
habbababbai <50295184+habbababbai@users.noreply.github.com>
2021-11-08 19:41:27 +0100

    commands register properly

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,5 +1,11 @@
 import { Event } from "../structures/Event";
+import { client } from "..";
 
 export default new Event ('ready', () => {
     console.log('Bot is online!');
+    const guild = client.guilds.cache.get(process.env.guildId as string);
+    client.slashCommands.forEach(command => {
+        guild?.commands.create(command);
+        console.log(`Created slash command: ${command.name}`);
+    })
 });
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,15 +1,14 @@
-import { Client, Intents, Collection, ApplicationCommandData, ApplicationCommandDataResolvable, ClientEvents } from "discord.js";
+import { Client, Intents, Collection, ApplicationCommandData, ApplicationCommandDataResolvable, ClientEvents, Guild } from "discord.js";
 import { CommandType } from "../typings/Command";
 import glob from 'glob';
 import { promisify } from "util";
-import { RegisterCommandsOptions } from "../typings/Client";
 import { Event } from "./Event";
 
 const globPromise = promisify(glob);
 
 export class ExtendedClient extends Client {
     commands: Collection<string, CommandType> = new Collection();
-
+    slashCommands:ApplicationCommandDataResolvable[] = [];
     constructor() {
         super({intents: [32767]});
     };
@@ -22,21 +21,10 @@ export class ExtendedClient extends Client {
     async importFile(filePath: string) {
         return (await import(filePath))?.default;
     }
-
-    async registerCommands({commands, guildId} : RegisterCommandsOptions) {
-        if (guildId) {
-            this.guilds.cache.get(guildId)?.commands.set(commands);
-            console.log(`Registering commands to ${guildId}`);
-        } else {
-            this.application?.commands.set(commands);
-            console.log("Registering global commands")
-
-        }
-    }
-
+    
     async registerModules() {
         // Commands
-        const slashCommands:ApplicationCommandDataResolvable[] = [];
+        
         const commandFiles = await globPromise(`${__dirname}/../commands/*/*{.ts,.js}`);
         
         commandFiles.forEach(async filePath => {
@@ -44,10 +32,10 @@ export class ExtendedClient extends Client {
             if (!command) return;
             console.log(command);
             this.commands.set(command.name, command);
-            slashCommands.push(command);
+            this.slashCommands.push(command);
         });
-        const options: RegisterCommandsOptions = { commands: slashCommands, guildId: process.env.guildId }
-        this.registerCommands(options);
+
+
 
         // Events
         const eventFiles = await globPromise(`${__dirname}/../events/*{.ts,.js}`);
